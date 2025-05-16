0cb800c98015d679d462bee7e67018bed55c8175
Ian Wafula <neocorpse@proton.me>
2025-05-16 11:55:08 +0300

    Still amazing

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,12 @@
 # your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, string):
+        self.string = string
+        
+    def parse(self):
+        myList = list(set(re.split(r',?\s|,\s?', self.string)))
+        myList.sort()
+        myList = [item for item in myList if re.match(r"^[A-z]\w+\.?\w+@[A-z]+\.[a-z]{2,3}", item)]
+        return myList
